6d6afada40f4841d3f3675c93ef95f55f0977127
Ramzi Carter Jr <Ramzi.carter@sjsu.edu>
2025-02-06 19:42:25 -0800

    New files added and modified README file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CS161
 Repo for Software Project
+
+
+
+
+**Hello This is a readMe File in markdown**
---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+Public class helloWorld
+{
+	Public static void main(String [] args)
+	{
+		System.out.println("HelloWorld");
+	}
+}
